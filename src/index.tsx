d8834b97790fefe688d74e1d21ee852bb466997d
Guy Ofeck <guyofeck@gmail.com>
2023-12-25 13:46:42 +0200

    wsr

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Dashboard from './components/Dashboard';
+import { WixDesignSystemProvider } from '@wix/design-system';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render(<Dashboard />);
+root.render(<WixDesignSystemProvider><Dashboard /></WixDesignSystemProvider>);
